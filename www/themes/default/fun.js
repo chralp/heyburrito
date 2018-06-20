97fceca2dacee62ed748fdda4ee2f024b34ee23c
Tobias Bleckert <hola@tobiasbleckert.se>
2018-06-20 22:09:18 +0200

    Quick pub sub implementation of burritos

--- www/themes/default/fun.js
+++ www/themes/default/fun.js
@@ -1,9 +1,8 @@
-
 socket.on('getUsers', (data) => {
     data.sort((a, b) => Math.sign(b.score - a.score));
     for (const a of data) {
         if (!$('.row').hasClass(a.username)) {
-            $('#content').append(`<tr class="row ${a.username}"><td class="avatar"><img src="${a.avatar}"></img></td><td class="name"> <p>${a.name}</p></td><td class="score"><p>${a.score}</p></td></tr>`);
+            $('#content').append(`<tr data-uuid="${a.username}" class="row"><td class="avatar"><img src="${a.avatar}"></img></td><td class="name"> <p>${a.name}</p></td><td class="score">${a.score}</td></tr>`);
         }
     }
 
@@ -11,6 +10,22 @@ socket.on('getUsers', (data) => {
 });
 
 
-socket.on('given', (data) => {
-    console.log(data);
+socket.on('GIVE', (data) => {
+    const $item = $('#content').find(`[data-uuid="${data}"]`);
+
+    if ($item.length) {
+        var burritos = parseInt($item.find('td.score').html(), 10);
+
+        $item.find('td.score').html(burritos + 1);
+    }
+});
+
+socket.on('TAKE_AWAY', (data) => {
+    const $item = $('#content').find(`[data-uuid="${data}"]`);
+
+    if ($item.length) {
+        var burritos = parseInt($item.find('td.score').html(), 10);
+
+        $item.find('td.score').html(burritos - 1);
+    }
 });
