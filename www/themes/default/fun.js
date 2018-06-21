b775d8b385d64800b3ea84308743bc7465b1267c
Christian Lind Alpstad <christian@alpstad.se>
2018-06-21 10:58:01 +0200

    fix of decrement, and adding promise

--- www/themes/default/fun.js
+++ www/themes/default/fun.js
@@ -1,31 +1,33 @@
-socket.on('getUsers', (data) => {
+
+function renderList(data){
     data.sort((a, b) => Math.sign(b.score - a.score));
     for (const a of data) {
         if (!$('.row').hasClass(a.username)) {
             $('#content').append(`<tr data-uuid="${a.username}" class="row"><td class="avatar"><img src="${a.avatar}"></img></td><td class="name"> <p>${a.name}</p></td><td class="score">${a.score}</td></tr>`);
         }
     }
+}
 
-    socket.emit('getRecivedLog', { username: 'USLACKBOT' });
+socket.on('getUsers', (data) => {
+    renderList(data)
 });
 
 
 socket.on('GIVE', (data) => {
-    const $item = $('#content').find(`[data-uuid="${data}"]`);
-
+    const $item = $('#content').find(`[data-uuid="${data[0].username}"]`);
     if ($item.length) {
         var burritos = parseInt($item.find('td.score').html(), 10);
-
-        $item.find('td.score').html(burritos + 1);
+        $item.find('td.score').html(data[0].score);
+    }else{
+        renderList(data)
     }
 });
 
 socket.on('TAKE_AWAY', (data) => {
-    const $item = $('#content').find(`[data-uuid="${data}"]`);
-
+    const $item = $('#content').find(`[data-uuid="${data[0].username}"]`);
     if ($item.length) {
         var burritos = parseInt($item.find('td.score').html(), 10);
 
-        $item.find('td.score').html(burritos - 1);
+        $item.find('td.score').html(data[0].score);
     }
 });
