32f715fd757dd7789a8f08902f4a4e1cc47e45ee
Christian Lind Alpstad <christian.alpstad@tt.se>
2018-09-29 22:53:07 +0200

    ggwp

--- app/web.ts
+++ app/web.ts
@@ -70,8 +70,6 @@ export default ((
         console.log(`server is listening on ${port}`);
     });
 
-    // const WebSocket = require('ws');
-
     const wss = new WebSocket.Server({ port: 8080 });
 
     wss.on('connection', function connection(ws:any) {
