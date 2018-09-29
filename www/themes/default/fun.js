d7c480cbe8f4a5cc5d9d7fb47899fd70f7dc1ed8
Tobias Bleckert <hola@tobiasbleckert.se>
2018-09-29 23:30:17 +0200

    Linting

--- www/themes/default/fun.js
+++ www/themes/default/fun.js
@@ -1,18 +1,9 @@
-/*
-* On start
-*/
-
-// Store where all users ?!
-const store = []
-
 // Get full list of received Burritos
-
 hey.on('open', function () {
-    hey.get('getReceivedList');
+    hey.get('getReceivedList', { type: 'burrito', sort: 'points', order: 'asc', take: 5, skip: 0 });
 });
 
 hey.on('receivedList', renderList);
-//hey.on('givenList', givenList);
 hey.on('userStats', userStats);
 
 hey.on('GIVE',GIVE)
@@ -22,39 +13,17 @@ function GIVE(data){
 /*
 * Special help functions
 */
-
-function returnProcent(totalScore,user){
-
-    a = totalScore
-    b = user
-    res = b / a
-    return res *100
+function returnPercent(totalScore,user) {
+    return (user / totalScore) * 100;
 }
 
 function openStats(user) {
     hey.get('getUserStats', user);
 }
 
-// Store First userList
-function fullList(data) {
-    console.log("FullList", data)
-    data.forEach(element => {
-        store.push(element)
-    });
-}
-
-function LocalStore(data) {
-    store.map(x => {
-        if(x.username === data[0].username){
-            return x.score = data[0].score
-        }
-    })
-    renderList(store)
-}
-
 function renderList(data) {
-    //data.sort((a, b) => Math.sign(b.score - a.score));
     $("#content").empty();
+
     for (const a of data) {
         if (!$('.row').hasClass(a.username)) {
             $('#content').append(`<tr data-uuid="${a.username}" onclick="openStats('${a.username}')" class="row"><td class="avatar"><img src="${a.avatar}"></img></td><td class="name"> <p>${a.name}</p></td><td class="score">${a.score}</td></tr>`);
@@ -62,51 +31,39 @@ function renderList(data) {
     }
 }
 
-//socket.on('GIVE', (data) => {
-function give(data) {
-    const $item = $('#content').find(`[data-uuid="${data[0].username}"]`);
-    if ($item.length) {
-        var burritos = parseInt($item.find('td.score').html(), 10);
-        $item.find('td.score').html(data[0].score);
-    }else{
-        renderList(data)
-    }
-}
-
 // Box, showUser stats
-function userStats (data){
-    console.log("data",data)
-    var x = document.getElementById("hiddenBox");
-    x.style.display = "block";
-    $("#head").empty();
-    $("#head").append(`<p><img src="${data.user.avatar}"></p>`)
-    $("#head").append(`<p>${data.user.name}</p>`)
+function userStats (data) {
+    console.log('data', data);
 
-    $("#stats").empty();
-    $("#stats").append(`<p>Total given: ${data.gived}</p>`)
-    $("#stats").append(`<p>Total received: ${data.user.score}</p>`)
-    $("#stats").append(`<br><br>`)
+    const x = document.getElementById('hiddenBox');
+    x.style.display = 'block';
 
-    if(data.givers.length){
+    $('#head').empty();
+    $('#head').append(`<p><img src="${data.user.avatar}"></p>`);
+    $('#head').append(`<p>${data.user.name}</p>`);
 
+    $('#stats').empty();
+    $('#stats').append(`<p>Total given: ${data.gived}</p>`);
+    $('#stats').append(`<p>Total received: ${data.user.score}</p>`);
+    $('#stats').append(`<br><br>`);
+
+    if (data.givers.length) {
         for (const a of data.givers) {
-            tjeeena = returnProcent(data.score,a.score)
+            const tjeeena = returnPercent(data.score,a.score);
             $('#stats').append(`<div class="progress"><p>${a.name}</p><div class="bar"><div class="bar-progress" style="height:24px;width:${tjeeena}%"></div></div>`);
         }
     }
 }
 
-
-
-
 // Hax to fix box on startup
-function close(){
-    var x = document.getElementById("hiddenBox");
-    if(x.style.display === "none"){
-        x.style.display = "block";
-    }else{
-        x.style.display = "none";
-    }
+function close() {
+    const x = document.getElementById('hiddenBox');
 
+    if(x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
 }
-close()
+
+close();
