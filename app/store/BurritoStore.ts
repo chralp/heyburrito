5e11d15dd87132dd8ee796346cd444b83bc424b9
Christian Lind Alpstad <christian@alpstad.se>
2019-01-25 20:57:24 +0100

    Adding getUserscore check, when running takeAway

--- app/store/BurritoStore.ts
+++ app/store/BurritoStore.ts
@@ -4,39 +4,49 @@ import UserScoreInterface from '../types/UserScore.interface';
 
 class BurritoStore extends EventEmitter {
 
-    database:any = null;
+    database: any = null;
 
     setDatabase(database) {
         this.database = database;
     }
 
-    givenBurritos(user:string) {
+    givenBurritos(user: string) {
         return this.database.findFrom(user);
     }
 
-    givenBurritosToday(user:string) {
+    givenBurritosToday(user: string) {
         return this.database.findFromToday(user);
     }
 
-    giveBurrito(to:string, from:string) {
+    giveBurrito(to: string, from: string) {
         log.info('Burrito given to %s from %s', to, from);
         return this.database.give(to, from).then(() => this.emit('GIVE', to));
     }
 
-    takeAwayBurrito(to:string, from:string) {
+    takeAwayBurrito(to: string, from: string) {
         log.info('Burrito taken away from %s by %s', to, from);
-        return this.database.takeAway(to, from).then(() => this.emit('TAKE_AWAY', to));
+        return this.getUserScore(to).then((data) => {
+
+            // Loweset score possible should be 0 ( ZERO )
+            const score = data.map(x => x.score)[0]
+
+            if (!score) return []
+
+            if (score > 0) {
+                return this.database.takeAway(to, from).then(() => this.emit('TAKE_AWAY', to));
+            }
+        })
     }
 
-    getUserScore(user:string = null): Promise<Array<UserScoreInterface>> {
+    getUserScore(user: string = null): Promise<Array<UserScoreInterface>> {
         return this.database.getScore(user);
     }
 
-    getGiven(user:string): Promise<Array<UserScoreInterface>> {
+    getGiven(user: string): Promise<Array<UserScoreInterface>> {
         return this.database.getGiven(user);
     }
 
-    getGivers(user:string): Promise<Array<UserScoreInterface>> {
+    getGivers(user: string): Promise<Array<UserScoreInterface>> {
         return this.database.getGivers(user);
     }
 }
