de268ef1b8dda8159f3dc525877fd82d56c87988
Christian Lind Alpstad <christian@alpstad.se>
2019-01-25 19:51:41 +0100

    tests

---
+++ declarations.d.ts
@@ -0,0 +1,12 @@
+/*
+* Declare JS modules that we want to use
+*/
+declare module 'bog';
+declare module 'dotenv';
+declare module 'path';
+declare module '@slack/client';
+declare module 'ws';
+// For tests
+declare module 'chai';
+declare module 'sinon';
+declare module 'fs';
