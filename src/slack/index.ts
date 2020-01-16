5ba3295ebb1492118246a1d0588ed0ac431d2223
Christian <christian@alpstad.se>
2020-01-16 17:14:09 +0100

    return object instead of function

--- src/slack/index.ts
+++ src/slack/index.ts
@@ -2,21 +2,13 @@ import * as log from 'bog';
 import { WebClient } from '@slack/web-api';
 import { RTMClient } from '@slack/rtm-api';
 import { RTMMock, WebMock } from '../../test/lib/slackMock';
-import RTMHandler from './Rtm';
-import WBCHandler from './Wbc';
 import config from '../config';
 
 const { slackMock } = config.misc;
 
 log.debug('Slack mockApi loaded', slackMock);
 
-export default () => {
-    const slackConf = {
-        rtm: slackMock ? new RTMMock() : new RTMClient(config.slack.api_token),
-        wbc: slackMock ? new WebMock() : new WebClient(config.slack.api_token),
-    };
-
-    slackConf.rtm.start();
-    RTMHandler.register(slackConf.rtm);
-    WBCHandler.register(slackConf.wbc);
+export default {
+    rtm: slackMock ? new RTMMock() : new RTMClient(config.slack.api_token),
+    wbc: slackMock ? new WebMock() : new WebClient(config.slack.api_token),
 };
