120feb932d7aac1297edc82c8077c06ac2fff1b8
Christian <christian@alpstad.se>
2020-01-19 02:06:19 +0100

    remove of newline

--- src/database/drivers/Driver.ts
+++ src/database/drivers/Driver.ts
@@ -15,7 +15,6 @@ interface Sum {
 }
 
 export default abstract class Driver {
-
     abstract async give(to: string, from: string, date): Promise<boolean>;
 
     abstract async takeAway(to: string, from: string, date): Promise<boolean>;
