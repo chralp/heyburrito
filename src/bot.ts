834c5f905ed9cd9111b4ca29121f69d1e8677c34
Christian <christian@alpstad.se>
2020-01-19 02:05:07 +0100

    export as functions

--- src/bot.ts
+++ src/bot.ts
@@ -12,22 +12,56 @@ interface Emojis {
     emoji: string;
 }
 
-const dailyCap: number = parseInt(config.slack.daily_cap);
+const dailyCap: number = Number(config.slack.daily_cap);
 const emojis: Array<Emojis> = [];
 
 if (config.slack.emoji_inc) {
     const incEmojis = config.slack.emoji_inc.split(', ');
     incEmojis.forEach((emoji: string) => emojis.push({ type: 'inc', emoji }));
-};
+}
 
 if (config.slack.emoji_dec) {
     const incEmojis = config.slack.emoji_dec.split(',');
     incEmojis.forEach((emoji: string) => emojis.push({ type: 'dec', emoji }));
+}
+
+const handleBurritos = async (giver: string, updates: any) => {
+    // Get given burritos today
+    const burritos = await BurritoStore.givenBurritosToday(giver, 'from');
+    log.info(`${giver} has given ${burritos} burritos today`);
+
+    const diff = dailyCap - burritos;
+
+    if (updates.length > diff) {
+        Wbc.sendDM(giver, `You are trying to give away ${updates.length} burritos, but you only have ${diff} burritos left today!`);
+        log.info(`User ${giver} is trying to give ${updates.length}, but u have only ${diff} left`);
+        return;
+    }
+
+    if (burritos >= dailyCap) {
+        log.info(`Daily cap of ${dailyCap} reached`);
+        return;
+    }
+
+    const a = updates.shift();
+
+    if (a.type === 'inc') {
+        await BurritoStore.giveBurrito(a.username, giver);
+        if (updates.length) {
+            handleBurritos(giver, updates);
+        }
+    } else if (a.type === 'dec') {
+        if (config.slack.enable_decrement) {
+            await BurritoStore.takeAwayBurrito(a.username, giver);
+        }
+
+        if (updates.length) {
+            handleBurritos(giver, updates);
+        }
+    }
 };
 
-
-export default () => {
-
+const start = () => {
     Rtm.on('slackMessage', (event: any) => {
         if (validMessage(event, emojis, LocalStore.getAllBots())) {
             if (validBotMention(event, LocalStore.botUserID())) {
@@ -39,49 +73,14 @@ export default () => {
                     const { giver, updates } = result;
                     if (updates.length) {
                         handleBurritos(giver, updates);
-                    };
-                };
-            };
-        };
+                    }
+                }
+            }
+        }
     });
+};
 
-    const handleBurritos = async (giver: string, updates) => {
-
-        // Get given burritos today
-        const burritos = await BurritoStore.givenBurritosToday(giver, 'from');
-        log.info(`${giver} has given ${burritos} burritos today`);
-
-        const diff = dailyCap - burritos
-
-        if (updates.length > diff) {
-            Wbc.sendDM(giver, `You are trying to give away ${updates.length} burritos, but you only have ${diff} burritos left today!`)
-            log.info(`User ${giver} is trying to give ${updates.length}, but u have only ${diff} left`)
-            return;
-        };
-
-        if (burritos >= dailyCap) {
-            log.info(`Daily cap of ${dailyCap} reached`);
-            return;
-        };
-
-        const a = updates.shift();
-
-
-        if (a.type === 'inc') {
-            await BurritoStore.giveBurrito(a.username, giver);
-            if (updates.length) {
-                handleBurritos(giver, updates);
-            };
-
-        } else if (a.type === 'dec') {
-
-            if (config.slack.enable_decrement) {
-                await BurritoStore.takeAwayBurrito(a.username, giver);
-            };
-
-            if (updates.length) {
-                handleBurritos(giver, updates);
-            };
-        };
-    };
-}
+export {
+    handleBurritos,
+    start,
+};
