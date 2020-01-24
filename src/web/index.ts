f2761ca2d14373ce0a9ef7c617cbb9c9067b76b4
Christian <christian@alpstad.se>
2020-01-24 08:07:28 +0100

    update

--- src/web/index.ts
+++ src/web/index.ts
@@ -25,31 +25,44 @@ export default (request: http.IncomingMessage, response: http.ServerResponse) =>
         '.css': 'text/css',
         '.json': 'application/json',
         '.png': 'image/png',
+        '.ico': 'image/x-icon',
         '.jpg': 'image/jpg',
     };
 
     const contentType: string = mimeTypes[extname] || 'application/octet-stream';
 
-    fs.readFile(filePath, 'utf-8', (error: any, content: any) => {
-        if (error) {
-            if (error.code === 'ENOENT') {
-                fs.readFile(`${publicPath}404.html`, (err: any, cont: any) => {
-                    if (err) log.warn('No 404 page found');
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.end(cont, 'utf-8');
-                });
+
+    if (contentType.startsWith('image')) {
+        fs.readFile(filePath, (err, content) => {
+            if (!err) {
+                response.writeHead(200, { 'Content-type': contentType });
+                response.end(content, 'utf-8');
+            }
+        });
+
+    } else {
+        fs.readFile(filePath, 'utf-8', (error: any, content: any) => {
+            if (error) {
+                if (error.code === 'ENOENT') {
+                    fs.readFile(`${publicPath}404.html`, (err: any, cont: any) => {
+                        if (err) log.warn('No 404 page found');
+                        response.writeHead(200, { 'Content-Type': 'text/html' });
+                        response.end(cont, 'utf-8');
+                    });
+                } else {
+                    response.writeHead(500);
+                    response.end(`Sorry, check with the site admin for error: ${error.code}  ..\n`);
+                    response.end();
+                }
             } else {
-                response.writeHead(500);
-                response.end(`Sorry, check with the site admin for error: ${error.code}  ..\n`);
-                response.end();
+                if (contentType === 'text/html') {
+                    const js: string = fs.readFileSync(`${libPath}Hey.js`, 'utf-8');
+                    content = content.replace('</head>', `<script>${js}</script></head>`);
+                }
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
             }
-        } else {
-            if (contentType === 'text/html') {
-                const js: string = fs.readFileSync(`${libPath}Hey.js`, 'utf-8');
-                content = content.replace('</head>', `<script>${js}</script></head>`);
-            }
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
+        });
+    }
+
 };
