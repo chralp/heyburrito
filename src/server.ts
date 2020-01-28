ef20e65c0d31aba20e2f9fc18923dc6e256d57de
Christian <christian@alpstad.se>
2020-01-28 20:14:24 +0100

    Wait for boot script before starting heyburrito

--- src/server.ts
+++ src/server.ts
@@ -16,55 +16,60 @@ import WEBHandler from './web';
 import WSSHandler from './wss';
 import boot from './lib/boot';
 
-boot();
-
-log.info('Staring heyburrito');
-
-// Configure BurritoStore
-BurritoStore.setDatabase(database);
-
-// Set and start slack services
-const { rtm, wbc } = slack;
-
-rtm.start();
-RTMHandler.register(rtm);
-WBCHandler.register(wbc);
-
-// Start bot instance
-start();
-
-// Start localstore instance
-LocalStore.start();
-
-/**
- * Httpserver request handler
- */
-const requestHandler = (request: http.IncomingMessage, response: http.ServerResponse) => {
-    /**
-     * Check if request url contains api path, then let APIHandler take care of it
-     */
-    if (request.url.includes(config.http.api_path)) return APIHandler(request, response);
-    /**
-     * Check if request url contains webpath, then let WEBHandler take care of it
-     */
-    if (request.url.includes(config.http.web_path)) return WEBHandler(request, response);
-    /**
-     * redirect all other requests to webPath
-     */
-    response.writeHead(301, {
-        location: config.http.web_path,
-    });
-    return response.end();
+const init = async () => {
+    await boot();
 };
 
-/**
- * Start HTTP / WSS server
- */
-const httpserver = http.createServer(requestHandler);
+init().then(() => {
 
-httpserver.listen(config.http.http_port, (err) => {
-    if (err) throw new Error(`Could not start HTTP server, error => ${err}`);
-    // Start WSS instance
-    WSSHandler();
-    log.info(`HttpServer started on ${config.http.http_port}`);
+    log.info('Staring heyburrito');
+
+    // Configure BurritoStore
+    BurritoStore.setDatabase(database);
+
+    // Set and start slack services
+    const { rtm, wbc } = slack;
+
+    rtm.start();
+    RTMHandler.register(rtm);
+    WBCHandler.register(wbc);
+
+    // Start bot instance
+    start();
+
+    // Start localstore instance
+    LocalStore.start();
+
+    /**
+     * Httpserver request handler
+     */
+    const requestHandler = (request: http.IncomingMessage, response: http.ServerResponse) => {
+        /**
+         * Check if request url contains api path, then let APIHandler take care of it
+         */
+        if (request.url.includes(config.http.api_path)) return APIHandler(request, response);
+        /**
+         * Check if request url contains webpath, then let WEBHandler take care of it
+         */
+        if (request.url.includes(config.http.web_path)) return WEBHandler(request, response);
+        /**
+         * redirect all other requests to webPath
+         */
+        response.writeHead(301, {
+            location: config.http.web_path,
+        });
+        return response.end();
+    };
+
+    /**
+     * Start HTTP / WSS server
+     */
+    const httpserver = http.createServer(requestHandler);
+
+    httpserver.listen(config.http.http_port, (err) => {
+        if (err) throw new Error(`Could not start HTTP server, error => ${err}`);
+        // Start WSS instance
+        WSSHandler();
+        log.info(`HttpServer started on ${config.http.http_port}`);
+    });
 });
