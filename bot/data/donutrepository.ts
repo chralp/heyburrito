bb39bc1f96d9dbf39791fea8efa21520a67a79f1
Maria Rekowska <mareko@microsoft.com>
2021-07-28 17:57:37 -0700

    put it together

---
+++ bot/data/donutrepository.ts
@@ -0,0 +1,10 @@
+import BurritoStore from "../../src/store/BurritoStore";
+import { ScoreAction, ScoreBoardRequest, ScoreBoardResult } from "../models/score";
+
+export async function addScores(scores: ScoreAction[]) {
+    
+}
+
+export async function getScoreboard(request: ScoreBoardRequest): Promise<ScoreBoardResult> {
+    throw "Not implemented"
+}
--- bot/dialogs/rootDialog.ts
+++ bot/dialogs/rootDialog.ts
@@ -1,4 +1,5 @@
 import { ComponentDialog, DialogContext } from "botbuilder-dialogs";
+import { addScores, getScoreboard } from "../data/donutrepository";
 import { ScoreBoardRequest } from "../models/score";
 import { createCardFromScoreboardResults, donutGivenConfirmationMesssage } from "./messageComposer/composer";
 import { parseScoreActions } from "./parser/actionParser";
@@ -23,63 +24,21 @@ export class RootDialog extends ComponentDialog {
     return await super.onContinueDialog(innerDc);
   }
 
-  async processTeamScoreboardRequest(innerDc: DialogContext, request: ScoreBoardRequest) {
-      const members = await getAllMembers(innerDc.context);
-      // TODO: iterate over members and all scores with parentContextId==teamId
-      console.log(members);
-  }
-
-  async processChatScoreboardRequest(innerDc: DialogContext, request: ScoreBoardRequest) {
-    const members = await getAllMembers(innerDc.context);
-    // TODO: iterate over members and all scores with id==chatId
-    console.log(members);
-  }
-
-  async processChannelScoreboardRequest(innerDc: DialogContext, request: ScoreBoardRequest) {
-    // TODO: channel vs team membership difference? 
-    // TODO: iterate over members and all scores with id==channelId
-    const members = await getAllMembers(innerDc.context);
-    console.log(members);
-  }
-
-  async processOrgTreeScoreboardRequest(innerDc: DialogContext, request: ScoreBoardRequest) {
-    throw "Not implemented"
-  }
-
-  async processGlobalScoreboardRequest(innerDc: DialogContext, request: ScoreBoardRequest) {
-    // TODO: get score for user
-    throw "Not implemented"
-  }
-
-  async processScoreboardRequest(innerDc: DialogContext, request: ScoreBoardRequest) {
-    switch (request.context.scope) {
-      case "team": return this.processChatScoreboardRequest(innerDc, request);
-      case "chat": return this.processTeamScoreboardRequest(innerDc, request);
-      case "channel": return this.processChannelScoreboardRequest(innerDc, request);
-      case "orgtree": return this.processOrgTreeScoreboardRequest(innerDc, request);
-      case "global": return this.processGlobalScoreboardRequest(innerDc, request);
-    }
-  }
-
   async triggerCommand(innerDc: DialogContext) {
 
     const activity = innerDc.context.activity;
 
     if (isScoreboardRequest(activity)) {
       const scoreBoardRequest = parseScoreboardRequest(activity);
-      // await this.processScoreboardRequest(innerDc, scoreBoardRequest);
-      const card = await createCardFromScoreboardResults(innerDc.context, {
-        request: scoreBoardRequest,
-        scores: [
-          {score: 3, userId: activity.from.id},
-          {score: 2, userId: activity.from.id},
-        ]});
+      const results = await getScoreboard(scoreBoardRequest);
+      const card = await createCardFromScoreboardResults(innerDc.context, results);
       await innerDc.context.sendActivity({ attachments: [card] });
       return await innerDc.cancelAllDialogs();
     }
 
     try {
       const actions = parseScoreActions(activity, true);
+      addScores(actions);
       const response = await donutGivenConfirmationMesssage(innerDc.context, actions);
       await innerDc.context.sendActivity(response);
       return await innerDc.cancelAllDialogs();
