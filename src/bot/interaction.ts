77e6c35d8683268404de5840379f4af75365825b
Christian Lind Alpstad <christian@alpstad.se>
2022-02-03 20:06:19 +0100

    added two commands to @heyburrito message

--- src/bot/interaction.ts
+++ src/bot/interaction.ts
@@ -1,19 +1,17 @@
 import Wbc from '../slack/Wbc';
 import { tmplHelp } from './tmpl/help';
+import { tmplEmoji } from './tmpl/emojis';
+
 export const BOT_INTERACTIONS = [
   'help',
-  'stats',
   'emojis'
 ];
-
-
 
 const parseCommands = (text) => {
   return BOT_INTERACTIONS.map((arg) => {
     if (text.includes(arg)) return arg;
   }).filter(s => s);
 };
-
 
 /**
  * Send message back to user or channel */
@@ -22,14 +20,21 @@ export const notifyUser = (user: string, message: any) => Wbc.sendDM(user, messa
 export const handleInteraction = async (message) => {
 
   const res = parseCommands(message.text);
-  if(!res.length) return false;
-  console.log(message)
+  if (!res.length) return false;
+
   res.forEach((command) => {
     switch (command) {
-      case 'help':
+      case 'help': {
         const data = tmplHelp();
         return notifyUser(message.username, data)
+      }
+      case 'emojis': {
+        const data = tmplEmoji();
+        return notifyUser(message.username, data)
+      }
+      default: {
+        return false;
+      }
     }
-
   });
 };
--- src/bot/tmpl/help.ts
+++ src/bot/tmpl/help.ts
@@ -1,17 +1,35 @@
 import { BOT_INTERACTIONS } from '../interaction';
+
 export const tmplHelp = () => {
+  const botTxt = BOT_INTERACTIONS.map((command) => {
+    return `*${command}*`;
+  }).filter(y => y).join().replace(/,/g, "\n");
+
   return {
     blocks: [
       {
         type: "section",
-        fields: BOT_INTERACTIONS.map((command) => {
-          if (command !== 'help') {
-            return {
-              type: 'plain_text',
-              text: `*${command}* \n`
-            }
-          }
-        }).filter(y => y)
+        text: {
+          type: 'mrkdwn',
+          text: "Available commands"
+        }
+      },
+      {
+        type: "section",
+        text: {
+          type: 'mrkdwn',
+          text: "@heyburrito <command>"
+        }
+      },
+      {
+        type: "divider"
+      },
+      {
+        type: "section",
+        text: {
+          type: 'mrkdwn',
+          text: botTxt
+        }
       }
     ]
   }
