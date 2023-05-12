d395e68cfbc617fc72e1da80600d242e909f374d
Jason Yoo <jasonyoo0515@gmail.com>
2023-05-12 16:04:16 +0900

    [FIX] 리액션 주는 사람이 국밥을 주도록 수정 (#3)

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,10 @@
-import config from './config';
-import BurritoStore from './store/BurritoStore';
-import LocalStore from './store/LocalStore';
-import { parseMessage } from './lib/parseMessage';
-import { validBotMention, validMessage, validReaction } from './lib/validator';
-import Rtm from './slack/Rtm';
-import Wbc from './slack/Wbc';
+import config from "./config";
+import BurritoStore from "./store/BurritoStore";
+import LocalStore from "./store/LocalStore";
+import { parseMessage } from "./lib/parseMessage";
+import { validBotMention, validMessage, validReaction } from "./lib/validator";
+import Rtm from "./slack/Rtm";
+import Wbc from "./slack/Wbc";
 
 const {
     enableDecrement,
@@ -26,20 +26,20 @@ interface Updates {
 }
 const emojis: Array<Emojis> = [];
 
-const incEmojis = emojiInc.split(',').map((emoji => emoji.trim()));
-incEmojis.forEach((emoji: string) => emojis.push({ type: 'inc', emoji }));
+const incEmojis = emojiInc.split(",").map((emoji) => emoji.trim());
+incEmojis.forEach((emoji: string) => emojis.push({ type: "inc", emoji }));
 
 if (!disableEmojiDec) {
-    const decEmojis = emojiDec.split(',').map((emoji => emoji.trim()));
-    decEmojis.forEach((emoji: string) => emojis.push({ type: 'dec', emoji }));
+    const decEmojis = emojiDec.split(",").map((emoji) => emoji.trim());
+    decEmojis.forEach((emoji: string) => emojis.push({ type: "dec", emoji }));
 }
 
 const giveBurritos = async (giver: string, updates: Updates[]) => {
     return updates.reduce(async (prev: any, burrito) => {
         return prev.then(async () => {
-            if (burrito.type === 'inc') {
+            if (burrito.type === "inc") {
                 await BurritoStore.giveBurrito(burrito.username, giver);
-            } else if (burrito.type === 'dec') {
+            } else if (burrito.type === "dec") {
                 await BurritoStore.takeAwayBurrito(burrito.username, giver);
             }
         });
@@ -50,10 +50,13 @@ const notifyUser = (user: string, message: string) => Wbc.sendDM(user, message);
 
 const handleBurritos = async (giver: string, updates: Updates[]) => {
     if (!enableDecrement) {
-        const burritos = await BurritoStore.givenBurritosToday(giver, 'from');
+        const burritos = await BurritoStore.givenBurritosToday(giver, "from");
         const diff = dailyCap - burritos;
         if (updates.length > diff) {
-            notifyUser(giver, `${updates.length} 개의 국밥을 주는데 실패했습니다. 오늘 줄 수 국밥의 개수는 ${diff}개 입니다.`);
+            notifyUser(
+                giver,
+                `${updates.length} 개의 국밥을 주는데 실패했습니다. 오늘 줄 수 국밥의 개수는 ${diff}개 입니다.`
+            );
             return false;
         }
         if (burritos >= dailyCap) {
@@ -61,22 +64,36 @@ const handleBurritos = async (giver: string, updates: Updates[]) => {
         }
         await giveBurritos(giver, updates);
     } else {
-        const givenBurritos = await BurritoStore.givenToday(giver, 'from', 'inc');
-        const givenRottenBurritos = await BurritoStore.givenToday(giver, 'from', 'dec');
-        const incUpdates = updates.filter((x) => x.type === 'inc');
-        const decUpdates = updates.filter((x) => x.type === 'dec');
+        const givenBurritos = await BurritoStore.givenToday(
+            giver,
+            "from",
+            "inc"
+        );
+        const givenRottenBurritos = await BurritoStore.givenToday(
+            giver,
+            "from",
+            "dec"
+        );
+        const incUpdates = updates.filter((x) => x.type === "inc");
+        const decUpdates = updates.filter((x) => x.type === "dec");
         const diffInc = dailyCap - givenBurritos;
         const diffDec = dailyDecCap - givenRottenBurritos;
         if (incUpdates.length) {
             if (incUpdates.length > diffInc) {
-                notifyUser(giver, `${updates.length} 개의 국밥을 주는데 실패했습니다. 오늘 줄 수 국밥의 개수는 ${diffInc}개 입니다.`);
+                notifyUser(
+                    giver,
+                    `${updates.length} 개의 국밥을 주는데 실패했습니다. 오늘 줄 수 국밥의 개수는 ${diffInc}개 입니다.`
+                );
             } else {
                 await giveBurritos(giver, incUpdates);
             }
         }
         if (decUpdates.length) {
             if (decUpdates.length > diffDec) {
-                notifyUser(giver, `You are trying to give away ${updates.length} rottenburritos, but you only have ${diffDec} rottenburritos left today!`);
+                notifyUser(
+                    giver,
+                    `You are trying to give away ${updates.length} rottenburritos, but you only have ${diffDec} rottenburritos left today!`
+                );
             } else {
                 await giveBurritos(giver, decUpdates);
             }
@@ -86,7 +103,7 @@ const handleBurritos = async (giver: string, updates: Updates[]) => {
 };
 
 const start = () => {
-    Rtm.on('slackMessage', async (event: any) => {
+    Rtm.on("slackMessage", async (event: any) => {
         if (validMessage(event, emojis, LocalStore.getAllBots())) {
             if (validBotMention(event, LocalStore.botUserID())) {
                 // Geather data and send back to user
@@ -102,22 +119,21 @@ const start = () => {
         }
     });
 
-    Rtm.on('slackReaction', async (event: any) => {
+    Rtm.on("slackReaction", async (event: any) => {
         if (validReaction(event)) {
-            const originalContent = await Wbc.fetchReactedMessage(event.item.channel, event.item.ts);
+            const originalContent = await Wbc.fetchReactedMessage(
+                event.item.channel,
+                event.item.ts
+            );
             const result = parseMessage(originalContent, emojis);
             if (result) {
-                const { giver, updates } = result;
+                const { updates } = result;
                 if (updates.length) {
-                    await handleBurritos(giver, updates);
+                    await handleBurritos(event.user, updates);
                 }
             }
         }
-    })
+    });
 };
 
-export {
-    handleBurritos,
-    notifyUser,
-    start,
-};
+export { handleBurritos, notifyUser, start };
