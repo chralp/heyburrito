217c4f8c980a8ac30d8dd4589fbacd2eb01f9100
Jason Yoo <jasonyoo0515@gmail.com>
2023-06-09 23:08:41 +0900

    [FIX] 셀프 리액션 덕 막기 (#15)

--- src/lib/parseMessage.ts
+++ src/lib/parseMessage.ts
@@ -75,8 +75,12 @@ function parseReactedMessage(reaction, reactedMsg, emojis) {
 
   // Get usernames from reacted slack message
   const users: string[] = parseUsernames(reactedMsg.text);
-  // If no one is mentioned on the original slack message, the sender receives duck
-  if (!users.length) {
+
+  // Filter self reaction
+  users.filter((u) => u !== reaction.user);
+
+  // If no one is mentioned on the original slack message and the sender is not the reactor, the sender receives ducks
+  if (!users.length && reaction.item_user != reaction.user) {
     users.push(reaction.item_user);
   }
 
