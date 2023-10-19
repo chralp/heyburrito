f6d8c8ec2c85e9e875f7bd3ac47ec179212670ce
White Piano <startoflateness@gmail.com>
2023-10-19 17:24:23 +0900

    basic config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
-    extends: ['airbnb-typescript/base'],
-    "rules": {
-        "max-len": ["warn", { "code": 140 }],
-        "quotes": [2, "single", { "avoidEscape": true }],
+    extends: ["airbnb-typescript/base", "prettier"],
+    reportUnusedDisableDirectives: true,
+    rules: {
+        "max-len": ["warn", { code: 140 }],
+        quotes: [2, "single", { avoidEscape: true }],
         "@typescript-eslint/indent": ["error", 4],
-
-    }
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { argsIgnorePattern: "^_", ignoreRestSiblings: true },
+        ],
+    },
 };
